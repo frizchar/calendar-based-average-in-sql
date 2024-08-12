8f39d0e46f85f60b1c4abd3d6bad6607ee4ef514
frizchar <cchrysanth@gmail.com>
2024-08-12 13:28:09 +0300

    Update calendar.sql

--- calendar.sql
+++ calendar.sql
@@ -1,15 +1,17 @@
 with d as ( 
- select distinct to_char(date_field,'mm') m, to_char(date_field,'yyyy') y 
+ select distinct                                    -- this subquery (d) gets month and year values from data_table
+  to_char(date_field,'mm') m, 
+  to_char(date_field,'yyyy') y 
  from data_table 
- where date_field >= '01/01/2023'                                                       -- calendar start date
+ where date_field >= '01/01/2023'                   -- calendar start date
 ),
 
 ds as (
- select 
+ select                                             -- this subquery (ds) calculates number of days per month based on subquery (d)
   m, y,
   to_date(m||'/'||y, 'mm/yyyy') s_date,
   last_day(to_date(m||'/'||y, 'mm/yyyy')) l_date,
-  last_day(to_date(m||'/'||y, 'mm/yyyy')) - to_date(m||'/'||y, 'mm/yyyy') + 1 nbr_days  -- calculate number of days per month
+  last_day(to_date(m||'/'||y, 'mm/yyyy')) - to_date(m||'/'||y, 'mm/yyyy') + 1 nbr_days  
  from d),
  
 sums as (
